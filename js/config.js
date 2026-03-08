5caac16e2100f47f30d464197ee196c6faf31e92
PawelSzmit <PawelSzmit@users.noreply.github.com>
2026-03-08 17:22:31 +0000

    deploy: 989d793deebda28783dd29b9628f092bf91731d2

---
+++ js/config.js
@@ -0,0 +1,7 @@
+/* ===========================================
+   Config - API keys (not tracked by git)
+   =========================================== */
+const CONFIG = {
+  GOOGLE_CLIENT_ID: '554823778989-760krqf91lrhq288s5l61oaa0fe2pekp.apps.googleusercontent.com',
+  GOOGLE_API_KEY: 'AIzaSyDDvMkVBAYKdLZnovxmtYTFfHUfVUG8MNM'
+};
